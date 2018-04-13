3f35fce98ffacc201d02e116e01def5ac6639bcf
ChaitanyaValluru <Valluru.Chaitanya@ad.infosys.com>
2018-04-13 18:10:12 +0530

    Add project page

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        messageDisp: 'Site updation'
-    });
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         messageDisp: 'Site updation'
+//     });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -49,6 +49,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
     });
 });
 
